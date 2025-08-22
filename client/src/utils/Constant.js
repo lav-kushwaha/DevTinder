3da92c17e53436e05a7be159810adb51dfa6ac13
Lav Kushwaha <lavkumar1224@gmail.com>
2025-08-23 02:24:27 +0530

    constant.js

--- client/src/utils/Constant.js
+++ client/src/utils/Constant.js
@@ -4,10 +4,10 @@
 // devtinder-backend-nu.vercel.app
 
 //dev
-// export const BASE_URL = location.hostname === "localhost"?"http://localhost:3000":"/api"
+export const BASE_URL = location.hostname === "localhost"?"http://localhost:3000":"/api"
 
-export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:3000"
-    : "https://devtinder-backend-alpha.vercel.app";
+// export const BASE_URL =
+//   location.hostname === "localhost"
+//     ? "http://localhost:3000"
+//     : "https://devtinder-backend-alpha.vercel.app";
 
